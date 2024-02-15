80188c5f295824f536f7c5695bb04847bdfb7cdb
OmarAliMSY <o.abdulmalik@mounting-systems.com>
2024-02-15 11:06:57 +0100

    Forcetool number of posts

--- script.py
+++ script.py
@@ -1,20 +1,23 @@
 import  matplotlib.pyplot as plt
 from FT.Forcetool import Forcetool
-
+import numpy as np
 
 
 torques= []
 forces,angles = [],[]
-ft = Forcetool(m=2400)
-
+ft = Forcetool(m=2400,west=False,push=True)
+ft.y = 70
 x = []
 y = []
 
-for i in range(-60,70,10): 
-    total_force = ft.latforce(west=False,push=False,theta=i)
+
+for i in range(-60,65,5): 
+    total_force = ft.latforce(theta=i)
     print(total_force)
-    ff = ft.get_torque_factor(force=total_force,push=False)
-    y.append( ((total_force * ff )))
+    
+    ff = ft.get_torque_factor(force=total_force)
+    y.append( (np.abs((total_force * ff ))))
+    
     x.append(i)
 
 plt.plot(x,y)
